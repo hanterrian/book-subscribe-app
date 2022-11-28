7db292d2f824f4fd2ccccb477f021a7d3e09e2b0
hanterrian <hanterrian@gmail.com>
2022-11-28 21:51:01 +0200

    Add login form

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
+import 'tw-elements';
+
 import './bootstrap';
+
+$(document).ready(function () {
+    $('.btn').data('mdb-ripple', 'true').data('mdb-ripple-color', 'light')
+});
--- resources/views/livewire/front/auth/login/form.blade.php
+++ resources/views/livewire/front/auth/login/form.blade.php
@@ -1,8 +1,16 @@
-<div>
-    <div>
-        <input type="text" wire:model="email"/>
-        <input type="password" wire:model="password"/>
+<div class="login_form">
+    <form>
+        <div class="form-group f-group">
+            <label class="form-label f-label" for="loginEmail">Email</label>
+            <input class="form-control f-control" id="loginEmail" type="text" wire:model="email"/>
+        </div>
+        <div class="form-group f-group">
+            <label class="form-label f-label" for="loginPassword">Password</label>
+            <input class="form-control f-control" id="loginPassword" type="password" wire:model="password"/>
+        </div>
         <input type="checkbox" wire:model="rememberMe"/>
-        <button wire:click="check" class="btn">Login</button>
-    </div>
+        <button type="button" class="btn" wire:click="check">
+            Login
+        </button>
+    </form>
 </div>
