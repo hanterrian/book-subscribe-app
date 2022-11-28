e562f3c68435f355040991979dea321a72b5cc99
hanterrian <hanterrian@gmail.com>
2022-11-28 16:07:07 +0200

    Add install script

--- resources/views/livewire/front/auth/login/form.blade.php
+++ resources/views/livewire/front/auth/login/form.blade.php
@@ -1,3 +1,8 @@
 <div>
-
+    <div>
+        <input type="text" wire:model="email"/>
+        <input type="password" wire:model="password"/>
+        <input type="checkbox" wire:model="rememberMe"/>
+        <button wire:click="check">Login</button>
+    </div>
 </div>
